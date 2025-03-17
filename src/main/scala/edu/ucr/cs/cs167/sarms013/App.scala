31caf6e9169ad9d0cfb82d236dfa00468f260de5
Sarah Armstrong <ghlasphyre@gmail.com>
2025-03-17 03:43:03 -0700

    task sections in App.scala

--- src/main/scala/edu/ucr/cs/cs167/sarms013/App.scala
+++ src/main/scala/edu/ucr/cs/cs167/sarms013/App.scala
@@ -4,12 +4,25 @@ package edu.ucr.cs.cs167.sarms013
  * @author ${user.name}
  */
 object App {
+
+  // TODO: Task 1: Preparation
+
+
+
+  // TODO: Task 2: Spatio-temporal analysis
+
+
+
+  // TODO: Task 3: Temporal analysis
+
+
+
+  // TODO: Task 4: Fire Intensity Prediction
+
+
+
+  // TODO: Task 5: Temporal analysis - 2
+
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
 
 }
