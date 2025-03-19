97c83b9a8f957afea0c20526fd3d3376e45410f0
Sarah Armstrong <ghlasphyre@gmail.com>
2025-03-18 23:27:52 -0700

    renamed App.scala to BeastScala.scala

--- src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
+++ src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
@@ -1,143 +1,296 @@
 package edu.ucr.cs.cs167.sarms013
 
-import edu.ucr.cs.bdlab.beast.geolite.{Feature, IFeature}
-import org.apache.spark.SparkConf
-import org.apache.spark.beast.SparkSQLRegistration
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
-import org.apache.spark.sql.functions._
+// /**
+//  * @author ${user.name}
+//  */
 
-import scala.collection.Map
+
+// General
+import org.apache.spark.SparkConf
+
+
+// Task4 4: Machine Learning Imports
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.tuning.CrossValidator
+// import org.apache.spark.mllib.regression.LinearRegressionModel
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.ml.classification.{LogisticRegression, LinearSVCModel}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{FeatureHasher, StandardScaler}
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit, TrainValidationSplitModel}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object BeastScala {
-  def main(args: Array[String]): Unit = {
-    // Initialize Spark context
-    val conf = new SparkConf().setAppName("Beast Example")
 
-    // Set Spark master to local if not already set
-    if (!conf.contains("spark.master"))
-      conf.setMaster("local[*]")
+    def main(args : Array[String]) {
+        
 
-    val spark: SparkSession.Builder = SparkSession.builder().config(conf)
-    val sparkSession: SparkSession = spark.getOrCreate()
-    val sparkContext = sparkSession.sparkContext
-    SparkSQLRegistration.registerUDT
-    SparkSQLRegistration.registerUDF(sparkSession)
 
-    //val operation: String = args(0)
-    val inputfile: String = args(0)
-    val countyName : String = args(1)
-    val countyFile = "tl_2018_us_county.zip"
-    try {
-      // Import Beast features
-      import edu.ucr.cs.bdlab.beast._
-      val t1 = System.nanoTime()
-      var validOperation = true
+        /*
+            Spark configuration; local or online.
+            Should be the same across tasks.
+        */
+        val conf = new SparkConf
+        if (!conf.contains("spark.master"))
+            conf.setMaster("local[*]")
+        println(s"Using Spark master '${conf.get("spark.master")}'")
 
-      // TODO: Temporal Analysis
-      var df : DataFrame = sparkSession.read.parquet(inputfile)
-      df.createOrReplaceTempView("wildfire_data")
-      //df.show()
-      //df.printSchema()
+        /*
+            SparkSession; enables usage of Spark apis.
+            Should be the same across tasks.
+        */
+        val spark = SparkSession
+            .builder()
+            .appName("CS167 Project")
+            .config(conf)
+            .getOrCreate()
 
-      var counties : DataFrame = sparkSession.read.format("shapefile").load(countyFile)
-      counties.createOrReplaceTempView("counties")
-      //counties.show()
+        /*
 
-      val countyGEOID = sparkSession.sql(
-          s"""
-            SELECT GEOID
-            FROM counties
-            WHERE NAME = "$countyName" AND STATEFP = "06"
-          """)
-      //countyGEOID.foreach(row => println(s"GEOID: ${row.getString(0)}"))
 
-//      val result = sparkSession.sql(
-//        s"""
-//           SELECT YEAR(acq_date) AS year, MONTH(acq_date) AS month, SUM(frp) AS total_fire_intensity
-//           FROM wildfire_data
-//           WHERE County = '$countyGEOID'
-//           GROUP BY YEAR(acq_date), MONTH(acq_date)
-//           ORDER BY year, month
-//        """)
-//
-//      result.show()
+            Note:
+            All tasks except Task 1 load the input file as parquet.
+            This might mean that we can't generalize reading the input
+            in this initial setup.
+        */
 
-      /*operation match {
-        case "count-by-county" =>
-          // Sample program arguments: count-by-county Tweets_1k.tsv
-          // TODO count the total number of tweets for each county and display on the screen
-          val tweetsDF = sparkSession.read.option("header", "true").option("delimiter", "\t").csv(inputFile)
-          //tweetsDF.show()
-          //tweetsDF.printSchema()
-          tweetsDF.selectExpr("*", "ST_CreatePoint(Longitude, Latitude) AS geometry")
-          val tweetsRDD: SpatialRDD = tweetsDF.selectExpr("*", "ST_CreatePoint(Longitude, Latitude) AS geometry").toSpatialRDD
-          val countiesRDD: SpatialRDD = sparkContext.shapefile("tl_2018_us_county.zip")
-          val countyTweet: RDD[(IFeature, IFeature)] = countiesRDD.spatialJoin(tweetsRDD)
-          val tweetsByCounty: Map[String, Long] = countyTweet
-            .map({ case (county, tweet) => (county.getAs[String]("NAME"), 1) })
-            .countByKey()
-          println("County\tCount")
-          for ((county, count) <- tweetsByCounty)
-            println(s"$county\t$count")
-        case "convert" =>
-          val outputFile = args(2)
-          // TODO add a CountyID column to the tweets, parse the text into keywords, and write back as a Parquet file
-          val tweetsRDD: SpatialRDD = sparkContext.readCSVPoint(inputFile,"Longitude","Latitude",'\t')
-          val countiesDF = sparkSession.read.format("shapefile").load("tl_2018_us_county.zip")
-          val countiesRDD: SpatialRDD = countiesDF.toSpatialRDD
-          val tweetCountyRDD: RDD[(IFeature, IFeature)] = tweetsRDD.spatialJoin(countiesRDD)
-          val tweetCounty: DataFrame = tweetCountyRDD.map({ case (tweet, county) => Feature.append(tweet, county.getAs[String]("GEOID"), "CountyID") }).toDataFrame(sparkSession)
-          //tweetCounty.printSchema()
-          val convertedDF: DataFrame = tweetCounty.selectExpr("CountyID", "split(lower(text), ',') AS keywords", "Timestamp")
-          //convertedDF.printSchema()
-          convertedDF.write.mode(SaveMode.Overwrite).parquet(outputFile)
-        case "count-by-keyword" =>
-          val keyword: String = args(2)
-          // TODO count the number of occurrences of each keyword per county and display on the screen
-          sparkSession.read.parquet(inputFile)
-            .createOrReplaceTempView("tweets")
-          println("CountyID\tCount")
-          sparkSession.sql(
-            s"""
-              SELECT CountyID, count(*) AS count
-              FROM tweets
-              WHERE array_contains(keywords, "$keyword")
-              GROUP BY CountyID
-            """).foreach(row => println(s"${row.get(0)}\t${row.get(1)}"))
-        case "choropleth-map" =>
-          val keyword: String = args(2)
-          val outputFile: String = args(3)
-          // TODO write a Shapefile that contains the count of the given keyword by county
-          sparkSession.read.parquet(inputFile)
-            .createOrReplaceTempView("tweets")
-          println("CountyID\tCount")
-          val keywordCountsdf = sparkSession.sql(
-            s"""
-              SELECT CountyID, count(*) AS count
-              FROM tweets
-              WHERE array_contains(keywords, "$keyword")
-              GROUP BY CountyID
-            """)
-          //.foreach(row => println(s"${row.get(0)}\t${row.get(1)}"))
-          keywordCountsdf.createOrReplaceTempView("keyword_counts")
-          sparkSession.read.format("shapefile").load("tl_2018_us_county.zip").createOrReplaceTempView("counties")
-          sparkSession.sql(
-            s"""
-              SELECT CountyID, NAME, geometry, count
-              FROM keyword_counts, counties
-              WHERE CountyID = GEOID
-            """).toSpatialRDD.coalesce(1).saveAsShapefile(outputFile)
-        case _ => validOperation = false
-      }
-      */
-      val t2 = System.nanoTime()
-      if (validOperation)
-        println(s"Operation on file '$inputfile' took ${(t2 - t1) * 1E-9} seconds")
-      else
-        Console.err.println(s"Invalid operation")
-    } finally {
-      sparkSession.stop()
+
+        try {
+            taskNumber match {
+
+                // TODO: Task 1: Preparation
+                case "1" =>
+
+
+
+
+                // TODO: Task 2: Spatio-temporal analysis
+                case "2" =>
+
+
+
+
+                // TODO: Task 3: Temporal analysis
+                case "3" =>
+
+
+
+
+                // TODO: Task 4: Fire Intensity Prediction
+                case "4" =>
+                    /*
+                        Command-line arguments. 
+                        Might need extra arguments depending on the task.
+                    */
+                    val taskNumber = args(0)
+                    val inputFile = args(1)
+
+                    val enableDebug = false
+                    val enableDBOut = false
+                    val enableBestO = false
+
+                    /*
+                        Setup DF
+                     */
+                    val startTime = System.nanoTime
+
+                    if (enableDebug) { System.out.println("Starting task 4...") }
+                    var df = spark.read.parquet(inputFile)
+                    var originalDF = df
+                    if (enableDebug) { System.out.println("[ReadParquet] Success!") } 
+
+                    df.createOrReplaceTempView("CurrentData")
+                    if (enableDBOut) { df.printSchema() }
+                    if (enableDBOut) { df.show(5) }
+                    if (enableDebug) { System.out.println("[CreateOrReplaceTempView] Success!") }
+
+                    df = spark.sql(
+                        s"""
+                           | SELECT
+                           |    AVG(A.ELEV_mean) AS ELEV_mean,
+                           |    AVG(A.SLP_mean) AS SLP_mean,
+                           |    AVG(A.EVT_mean) AS EVT_mean,
+                           |    AVG(A.EVH_mean) AS EVH_mean,
+                           |    AVG(A.CH_mean) AS CH_mean,
+                           |    AVG(A.TEMP_ave) AS TEMP_ave,
+                           |    AVG(A.TEMP_min) AS TEMP_min,
+                           |    AVG(A.TEMP_max) AS TEMP_max,
+                           |    SUM(A.frp) AS fire_intensity,
+                           |    (SELECT YEAR(A.acq_date)) AS Year,
+                           |    (SELECT MONTH(A.acq_date)) AS Month,
+                           |    A.County
+                           | FROM CurrentData A
+                           | GROUP BY
+                           |    A.County,
+                           |    Year,
+                           |    Month
+                           |""".stripMargin)
+                    if (enableDBOut) { df.printSchema() }
+                    if (enableDBOut) { df.show(5) }
+                    if (enableDebug) { System.out.println("[SQL] Success!")}
+
+
+                    /*
+                        Transformations
+                     */
+                    val inputColumns = Array(
+                        "ELEV_mean", "SLP_mean", "EVT_mean", 
+                        "EVH_mean", "CH_mean", "TEMP_ave", 
+                        "TEMP_min", "TEMP_max"
+                    )
+                    val assembledVector = new VectorAssembler()
+                        .setInputCols(inputColumns)
+                        .setOutputCol("attributes")
+                    val standardScaler = new StandardScaler()
+                        .setInputCol("attributes")
+                        .setOutputCol("features")
+                    if (enableDebug) { System.out.println("[CreatedTransforms] Success!") }
+
+                    val linReg = new LinearRegression()
+                        .setFeaturesCol("features")
+                        .setLabelCol("fire_intensity")
+                        .setMaxIter(1000)
+                    if (enableDebug) { System.out.println("[CreatedLogisticRegressionClassifier] Success!") }
+
+
+                    /*
+                        Pipeline
+                    */
+                    val pipeline = new Pipeline()
+                        .setStages(Array(
+                            assembledVector,
+                            standardScaler,
+                            linReg
+                        ))
+                    if (enableDebug) { System.out.println("[CreatedPipeline] Success!") }
+
+
+                    val paramGrid: Array[ParamMap] = new ParamGridBuilder()
+                        .addGrid(linReg.fitIntercept, Array(true,false))
+                        .addGrid(linReg.elasticNetParam, Array(0.0, 0.3, 0.8, 1.0))
+                        .addGrid(linReg.regParam,	Array(0.01, 0.0001))
+                        .addGrid(linReg.maxIter,	Array(10, 100))
+                        .addGrid(linReg.tol,	Array(0.0001, 0.01))
+                        .build()
+                    if (enableDebug) { System.out.println("[CreatedParamGrid] Success!")}
+
+
+                    /*
+                        Evaluators
+                     */
+                    val regressionEvaluator = new RegressionEvaluator()
+                        .setLabelCol("fire_intensity")
+                        .setPredictionCol("prediction")
+                        .setMetricName("rmse")
+                    val chosenEvaluator = regressionEvaluator
+                    if (enableDebug) { System.out.println("[CreatedAndChoseEvaluator] Success!")}
+
+
+                    /*
+                        Validators
+                     */
+                    val trainValidator = new TrainValidationSplit()
+                        .setEstimator(pipeline)
+                        .setEvaluator(chosenEvaluator)
+                        .setEstimatorParamMaps(paramGrid)
+                        .setTrainRatio(0.8)
+                        .setParallelism(2)
+                    val crossValidator = new CrossValidator()
+                        .setEstimator(pipeline)
+                        .setEvaluator(chosenEvaluator)
+                        .setEstimatorParamMaps(paramGrid)
+                        .setNumFolds(5)
+                        .setParallelism(2)
+                    val chosenValidator = crossValidator
+                    if (enableDebug) { System.out.println("[CreatedAndChoseValidator] Success!")}
+
+                    if (enableDBOut) { df.show(5) }
+                    val Array(trainingData: Dataset[Row], testData: Dataset[Row]) = df.randomSplit(Array(0.8, 0.2))
+                    if (enableDebug) { System.out.println(trainingData)}
+                    if (enableDebug) { System.out.println("[SplitData] Success!")}
+
+                    val model = chosenValidator.fit(trainingData)
+                    if (enableDebug) { System.out.println("[RanCrossValidation] Success!")}
+
+
+                    /*
+                        Best Parameters
+                    */
+                    if (enableBestO) {
+                        val fitIntercept = model.bestModel
+                            .asInstanceOf[PipelineModel]
+                            .stages(2)
+                            .asInstanceOf[LinearRegressionModel]
+                            .fitIntercept
+                        val netParam = model.bestModel
+                            .asInstanceOf[PipelineModel]
+                            .stages(2)
+                            .asInstanceOf[LinearRegressionModel]
+                            .elasticNetParam
+                        val regParam = model.bestModel
+                            .asInstanceOf[PipelineModel]
+                            .stages(2)
+                            .asInstanceOf[LinearRegressionModel]
+                            .regParam
+                        val maxIter = model.bestModel
+                            .asInstanceOf[PipelineModel]
+                            .stages(2)
+                            .asInstanceOf[LinearRegressionModel]
+                            .maxIter
+                        val tol = model.bestModel
+                            .asInstanceOf[PipelineModel]
+                            .stages(2)
+                            .asInstanceOf[LinearRegressionModel]
+                            .tol
+
+
+                        System.out.println("--Parameters of the best Model--")
+                        System.out.println("fitIntercept: ", fitIntercept)
+                        System.out.println("elasticNetParam: ", netParam)
+                        System.out.println("regParam: ", regParam)
+                        System.out.println("maxIter: ", maxIter)
+                        System.out.println("Param: ", tol)
+                        System.out.println()
+                    }
+                    if (enableBestO) { System.out.println("[ChoosingBestModel] Success!")}
+
+
+                    /*
+                        Running and Evaluation
+                    */
+                    val finalRegressionEvaluator = new RegressionEvaluator()
+                        .setLabelCol("fire_intensity")
+                        .setPredictionCol("prediction")
+                        .setMetricName("rmse")
+
+                    val predictions: DataFrame = model.transform(testData)
+                    predictions.select(
+                        "ELEV_mean", "SLP_mean", "EVT_mean",
+                        "EVH_mean", "CH_mean", "TEMP_ave",
+                        "TEMP_min", "TEMP_max", "fire_intensity", "prediction"
+                    ).show()
+                    if (enableDebug) { System.out.println("# of predictions: ", predictions.count()) }
+                    if (enableDBOut) { System.out.println("Original DF: "); originalDF.show() }
+                    if (enableDBOut) { System.out.println("Scaled DF: "); df.show() }
+
+                    val rsmeResult: Double = finalRegressionEvaluator.evaluate(predictions)
+                    System.out.println(s"RMSE of the test set is $rsmeResult")
+
+                    val endTime = System.nanoTime
+                    System.out.println("Total Execution Time: " + (endTime - startTime) * 1E-9 + " seconds")
+
+
+                // TODO: Task 5: Temporal analysis - 2
+                case "5" =>
+            }
+        }
+        
     }
-  }
+
+
+  
+
 }
