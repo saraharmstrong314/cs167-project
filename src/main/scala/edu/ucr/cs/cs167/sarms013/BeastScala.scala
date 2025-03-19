942dcadf9836e854de48802c587a012f9d5adcdd
Sarah Armstrong <ghlasphyre@gmail.com>
2025-03-18 22:57:11 -0700

    finished task

--- src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
+++ src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
@@ -37,100 +37,43 @@ object BeastScala {
       // TODO: Temporal Analysis
       var df : DataFrame = sparkSession.read.parquet(inputfile)
       df.createOrReplaceTempView("wildfire_data")
-      //df.show()
-      //df.printSchema()
 
       var counties : DataFrame = sparkSession.read.format("shapefile").load(countyFile)
       counties.createOrReplaceTempView("counties")
-      //counties.show()
 
       val countyGEOID = sparkSession.sql(
           s"""
             SELECT GEOID
             FROM counties
             WHERE NAME = "$countyName" AND STATEFP = "06"
-          """)
-      //countyGEOID.foreach(row => println(s"GEOID: ${row.getString(0)}"))
+          """).collect().head.getString(0)
 
-//      val result = sparkSession.sql(
-//        s"""
-//           SELECT YEAR(acq_date) AS year, MONTH(acq_date) AS month, SUM(frp) AS total_fire_intensity
-//           FROM wildfire_data
-//           WHERE County = '$countyGEOID'
-//           GROUP BY YEAR(acq_date), MONTH(acq_date)
-//           ORDER BY year, month
-//        """)
-//
-//      result.show()
+      // acq_date column is of type string, need to be date type and put leading 0 for months < 10
+      val cleanedData = df.withColumn("acq_date", to_date(col("acq_date"), "yyyy-MM-dd"))
+        .withColumn("year", year(col("acq_date")))
+        .withColumn("month", month(col("acq_date")))
+        .withColumn("month", lpad(col("month").cast("string"), 2, "0"))
+        .withColumn("year_month", concat_ws("-", col("year"), col("month")))
 
-      /*operation match {
-        case "count-by-county" =>
-          // Sample program arguments: count-by-county Tweets_1k.tsv
-          // TODO count the total number of tweets for each county and display on the screen
-          val tweetsDF = sparkSession.read.option("header", "true").option("delimiter", "\t").csv(inputFile)
-          //tweetsDF.show()
-          //tweetsDF.printSchema()
-          tweetsDF.selectExpr("*", "ST_CreatePoint(Longitude, Latitude) AS geometry")
-          val tweetsRDD: SpatialRDD = tweetsDF.selectExpr("*", "ST_CreatePoint(Longitude, Latitude) AS geometry").toSpatialRDD
-          val countiesRDD: SpatialRDD = sparkContext.shapefile("tl_2018_us_county.zip")
-          val countyTweet: RDD[(IFeature, IFeature)] = countiesRDD.spatialJoin(tweetsRDD)
-          val tweetsByCounty: Map[String, Long] = countyTweet
-            .map({ case (county, tweet) => (county.getAs[String]("NAME"), 1) })
-            .countByKey()
-          println("County\tCount")
-          for ((county, count) <- tweetsByCounty)
-            println(s"$county\t$count")
-        case "convert" =>
-          val outputFile = args(2)
-          // TODO add a CountyID column to the tweets, parse the text into keywords, and write back as a Parquet file
-          val tweetsRDD: SpatialRDD = sparkContext.readCSVPoint(inputFile,"Longitude","Latitude",'\t')
-          val countiesDF = sparkSession.read.format("shapefile").load("tl_2018_us_county.zip")
-          val countiesRDD: SpatialRDD = countiesDF.toSpatialRDD
-          val tweetCountyRDD: RDD[(IFeature, IFeature)] = tweetsRDD.spatialJoin(countiesRDD)
-          val tweetCounty: DataFrame = tweetCountyRDD.map({ case (tweet, county) => Feature.append(tweet, county.getAs[String]("GEOID"), "CountyID") }).toDataFrame(sparkSession)
-          //tweetCounty.printSchema()
-          val convertedDF: DataFrame = tweetCounty.selectExpr("CountyID", "split(lower(text), ',') AS keywords", "Timestamp")
-          //convertedDF.printSchema()
-          convertedDF.write.mode(SaveMode.Overwrite).parquet(outputFile)
-        case "count-by-keyword" =>
-          val keyword: String = args(2)
-          // TODO count the number of occurrences of each keyword per county and display on the screen
-          sparkSession.read.parquet(inputFile)
-            .createOrReplaceTempView("tweets")
-          println("CountyID\tCount")
-          sparkSession.sql(
-            s"""
-              SELECT CountyID, count(*) AS count
-              FROM tweets
-              WHERE array_contains(keywords, "$keyword")
-              GROUP BY CountyID
-            """).foreach(row => println(s"${row.get(0)}\t${row.get(1)}"))
-        case "choropleth-map" =>
-          val keyword: String = args(2)
-          val outputFile: String = args(3)
-          // TODO write a Shapefile that contains the count of the given keyword by county
-          sparkSession.read.parquet(inputFile)
-            .createOrReplaceTempView("tweets")
-          println("CountyID\tCount")
-          val keywordCountsdf = sparkSession.sql(
-            s"""
-              SELECT CountyID, count(*) AS count
-              FROM tweets
-              WHERE array_contains(keywords, "$keyword")
-              GROUP BY CountyID
-            """)
-          //.foreach(row => println(s"${row.get(0)}\t${row.get(1)}"))
-          keywordCountsdf.createOrReplaceTempView("keyword_counts")
-          sparkSession.read.format("shapefile").load("tl_2018_us_county.zip").createOrReplaceTempView("counties")
-          sparkSession.sql(
-            s"""
-              SELECT CountyID, NAME, geometry, count
-              FROM keyword_counts, counties
-              WHERE CountyID = GEOID
-            """).toSpatialRDD.coalesce(1).saveAsShapefile(outputFile)
-        case _ => validOperation = false
-      }
-      */
+      cleanedData.createOrReplaceTempView("wildfire_data")
+
+      val result = sparkSession.sql(
+        s"""
+           SELECT year, month, SUM(frp) AS total_fire_intensity
+           FROM wildfire_data
+           WHERE County = '$countyGEOID'
+           GROUP BY year, month
+           ORDER BY year, month
+        """)
+
+      val formattedResult = result.withColumn(
+        "year_month",
+        concat_ws("-", col("year"), lpad(col("month").cast("string"), 2, "0"))
+      ).select("year_month", "total_fire_intensity")
+
+      val outputFile = s"wildfires_$countyName.csv"
+      formattedResult.write.option("header", "true").csv(outputFile)
+
       val t2 = System.nanoTime()
       if (validOperation)
         println(s"Operation on file '$inputfile' took ${(t2 - t1) * 1E-9} seconds")
