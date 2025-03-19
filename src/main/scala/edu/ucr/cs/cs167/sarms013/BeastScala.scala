a4319810a0b0d2ca1d24946925dfc592d17df6fb
Sarah Armstrong <ghlasphyre@gmail.com>
2025-03-18 23:44:23 -0700

    all tasks merged

--- src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
+++ src/main/scala/edu/ucr/cs/cs167/sarms013/BeastScala.scala
@@ -28,6 +28,16 @@ import org.apache.spark.beast.SparkSQLRegistration
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
+import edu.ucr.cs.bdlab.beast.JavaSpatialSparkContext
+import org.apache.spark.beast.{CRSServer, SparkSQLRegistration}
+import org.apache.spark.{SparkConf}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import edu.ucr.cs.bdlab.beast.geolite.{Feature, IFeature}
+import org.apache.spark.SparkConf
+import org.apache.spark.beast.SparkSQLRegistration
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
 
 object BeastScala {
 
@@ -70,13 +80,181 @@ object BeastScala {
                 // TODO: Task 1: Preparation
                 case "1" =>
 
+                    if (args.length < 3) {
+                        System.err.println("Usage: WildfireDataPreparation <wildfireCSV> <countyShapefileZip> <outputParquet>")
+                        System.exit(1)
+                    }
 
+                    val wildfireCSV = args(0)
+                    val countyZip = args(1)
+                    val outputParquet = args(2)
 
+                    // 1. Create SparkConf and SparkSession
+                    val conf = new SparkConf()
+                      .setAppName("Wildfire Data Preparation (Spark + Beast, Scala)")
+                      .setMaster("local[*]")
+
+                    val spark: SparkSession = SparkSession.builder()
+                      .config(conf)
+                      .getOrCreate()
+
+                    // 2. Create Beast JavaSpatialSparkContext (old Beast style)
+                    val sparkContext = new JavaSpatialSparkContext(spark.sparkContext)
+
+                    // 3. (Optional) Start the CRS server if you need transformations
+                    CRSServer.startServer(sparkContext)
+
+                    // 4. Register geometry UDT and UDFs (ST_Point, ST_Contains, etc.)
+                    SparkSQLRegistration.registerUDT()
+                    SparkSQLRegistration.registerUDF(spark)
+
+                    // 5. Load Wildfire CSV
+                    //    If it's tab-delimited, we specify "delimiter" -> "\t".
+                    val wildfiresRaw: Dataset[Row] = spark.read
+                      .option("header", "true")
+                      .option("inferSchema", "false")
+                      .option("delimiter", "\t")
+                      .csv(wildfireCSV)
+
+                    // 6. Keep only the needed columns
+                    var wildfires = wildfiresRaw.select(
+                        col("x"),
+                        col("y"),
+                        col("acq_date"),
+                        col("frp"),
+                        col("acq_time"),
+                        col("ELEV_mean"),
+                        col("SLP_mean"),
+                        col("EVT_mean"),
+                        col("EVH_mean"),
+                        col("CH_mean"),
+                        col("TEMP_ave"),
+                        col("TEMP_min"),
+                        col("TEMP_max")
+                    )
+
+                    // 7. Fix the 'frp' column: "12.3,45.7" -> take "12.3" -> cast to double
+                    wildfires = wildfires.withColumn(
+                        "frp",
+                        expr("double(split(frp, ',')[0])")
+                    )
+
+                    // 8. Cast numeric columns to double
+                    val numericCols = Array(
+                        "x", "y", "frp",
+                        "ELEV_mean", "SLP_mean", "EVT_mean", "EVH_mean",
+                        "CH_mean", "TEMP_ave", "TEMP_min", "TEMP_max"
+                    )
+
+                    numericCols.foreach { c =>
+                        wildfires = wildfires.withColumn(c, col(c).cast("double"))
+                    }
+
+                    // 9. Create geometry column using ST_CreatePoint(x, y) (if your version has it)
+                    //    If you get an error about ST_CreatePoint, try ST_Point or ST_GeomFromText
+                    wildfires = wildfires.withColumn(
+                        "geometry",
+                        expr("ST_CreatePoint(x, y)")
+                    )
+
+                    // 10. Load the county shapefile
+                    val counties = spark.read
+                      .format("shapefile")
+                      .load(countyZip)
+                      .select(
+                          col("geometry").alias("county_geom"),
+                          col("GEOID")
+                      )
+
+                    // 11. Create temp views for SQL
+                    wildfires.createOrReplaceTempView("wildfires")
+                    counties.createOrReplaceTempView("counties")
+
+                    // 12. Spatial join with ST_Contains
+                    val joinSQL =
+                        s"""
+                           |SELECT
+                           |  w.x, w.y, w.acq_date, w.frp, w.acq_time,
+                           |  w.ELEV_mean, w.SLP_mean, w.EVT_mean, w.EVH_mean,
+                           |  w.CH_mean, w.TEMP_ave, w.TEMP_min, w.TEMP_max,
+                           |  c.GEOID AS County, w.geometry
+                           |FROM wildfires w JOIN counties c
+                           |ON ST_Contains(c.county_geom, w.geometry)
+                           |""".stripMargin
+
+                    val joined = spark.sql(joinSQL)
+
+                    // 13. Drop geometry column
+                    val finalDF = joined.drop("geometry")
+
+                    // 14. Write Parquet
+                    finalDF.write.mode("overwrite").parquet(outputParquet)
+
+                    println(s"Done! Created Parquet at: $outputParquet")
+
+                    spark.stop()
 
                 // TODO: Task 2: Spatio-temporal analysis
                 case "2" =>
 
+                    if (args.length != 3) {
+                        println("Usage: WildfireSpatioTemporalAnalysis <start_date> <end_date> <output_directory>")
+                        sys.exit(1)
+                    }
 
+                    val startDate = args(0)  // "01/01/2016"
+                    val endDate = args(1)    // "12/31/2017"
+                    val outputDir = args(2)  // Output directory for Shapefile
+
+                    val conf = new SparkConf().setAppName("WildfireSpatioTemporalAnalysis").setMaster("local[*]")
+                    val spark = SparkSession.builder().config(conf).getOrCreate()
+
+                    SparkSQLRegistration.registerUDT()
+                    SparkSQLRegistration.registerUDF(spark)
+
+                    // Load wildfire dataset
+                    val wildfireDF = spark.read.parquet("wildfiredb_sample.parquet")
+                    wildfireDF.createOrReplaceTempView("wildfire_data")
+
+                    //wildfireDF.show(5);
+
+                    // Load county dataset (Shapefile)
+                    val countyDF = spark.read.format("shapefile").load("tl_2018_us_county.zip")
+                    countyDF.createOrReplaceTempView("counties")
+
+                    //countyDF.show(5);
+
+                    // Query to filter and sum fire intensity
+                    val filteredWildfires = spark.sql(
+                        s"""
+                           |SELECT County AS county_name, SUM(frp) AS fire_intensity
+                           |FROM wildfire_data
+                           |WHERE to_date(acq_date, 'yyyy-MM-dd')
+                           |      BETWEEN to_date('$startDate', 'MM/dd/yyyy')
+                           |      AND to_date('$endDate', 'MM/dd/yyyy')
+                           |GROUP BY County
+                        """.stripMargin
+                    )
+                    filteredWildfires.createOrReplaceTempView("fire_intensity_data")
+
+                    //filteredWildfires.show(5);
+
+                    // Ensure `County` in wildfire_data matches `NAME` or `GEOID` in `counties`
+                    val joinedDF = spark.sql(
+                        s"""
+                           |SELECT counties.GEOID, counties.NAME, counties.geometry AS g, fire_intensity
+                           |FROM fire_intensity_data
+                           |JOIN counties ON fire_intensity_data.county_name = counties.GEOID
+                        """.stripMargin
+                    )
+
+                    //joinedDF.show(5);
+                    // Output to Shapefile for QGIS visualization
+                    joinedDF.coalesce(1).write.format("shapefile").save(outputDir)
+
+                    println(s"Choropleth map data saved to $outputDir")
+
+                    spark.stop()
 
 
                 // TODO: Task 3: Temporal analysis
@@ -361,6 +539,45 @@ object BeastScala {
 
                 // TODO: Task 5: Temporal analysis - 2
                 case "5" =>
+
+                    val spark = SparkSession.builder()
+                      .appName("Wildfire Temporal Analysis")
+                      .master("local[*]") // Run Spark locally with all available cores
+                      .config("spark.sql.parquet.enableVectorizedReader", "false")
+                      .getOrCreate()
+
+                    import spark.implicits._
+
+                    // Hardcoded start and end dates
+                    val startDate = "01-01-2020"
+                    val endDate = "12-31-2020"
+
+                    // Load county dataset and filter for California (STATEFP = "06")
+                    val df = spark.read.parquet("C:/Users/jjin1/CS167/workspace/jjin060_project2/tl_2018_us_county.parquet")
+                      .filter($"STATEFP" === "06")
+                      .select($"GEOID", $"NAME".alias("county_name"))
+
+                    // Load wildfire dataset
+                    val wildfireDF = spark.read.format("parquet").load("C:/Users/jjin1/CS167/workspace/jjin060_project2/wildfiredb_10k.parquet")
+                      .withColumn("acq_date", to_date(col("acq_date"), "yyyy-MM-dd"))
+                      .filter(col("acq_date").between(to_date(lit(startDate), "MM-dd-yyyy"), to_date(lit(endDate), "MM-dd-yyyy")))
+
+                    // Perform an equi-join on GEOID = County
+                    val countyDF = spark.read.parquet("C:/Users/jjin1/CS167/workspace/jjin060_project2/tl_2018_us_county.parquet")
+                    val joinedDF = wildfireDF.join(countyDF, wildfireDF("County") === countyDF("GEOID"))
+
+                    // Aggregate total fire intensity per county
+                    val resultDF = joinedDF.groupBy("county_name")
+                      .agg(sum("frp").alias("total_fire_intensity"))
+                      .orderBy(desc("total_fire_intensity"))
+
+                    // Save the result as CSV
+                    resultDF.coalesce(1).write.option("header", "true").csv("/output/wildfires_California.csv")
+
+                    // Display results
+                    resultDF.show(10, false)
+
+                    spark.stop()
             }
         }
         
